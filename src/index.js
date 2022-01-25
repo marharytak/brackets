8278c94c931ac1258a1088efaf3bf1f20593a06f
Margarita Korzun <margarita.korzun@internetfusion.co.uk>
2022-01-25 13:55:24 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const isConfigNum = Number.isInteger(+bracketsConfig[0][0]);
+  let txtToReplace = '';
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const opening = bracketsConfig[i][0];
+    const closing = bracketsConfig[i][1];
+    txtToReplace += isConfigNum
+      ? `[${opening}-${closing}]{2}`
+      : `(\\${opening}\\${closing})`;
+    if (i !== bracketsConfig.length - 1) { txtToReplace += '|'; }
+  }
+
+  const reg = new RegExp(txtToReplace, 'g');
+  while (str.match(reg)) { str = str.replace(reg, ''); }
+  return !str.length;
+};
